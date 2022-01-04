83fc999a6374f3a0fbb0340247aac723bb9f8485
Gary Lee <dokdo2005@gmail.com>
2022-01-05 02:17:50 +0900

    편지 열기 버튼 추가

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ const App = () => {
 
   return (
     <div className='mainContainer'>
-      <img src={letterIcon} width={'65%'} onClick={() => setVisibility(true)} />
+      <img src={letterIcon} width={'65%'} />
       <div>&nbsp;</div>
       <div className='mainText'>모험가의 편지가 도착했어요!</div>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
+      <button className='letterOpenBtn' onClick={() => setVisibility(true)}>
+        <p className='letterOpenBtnText'>열어보기</p>
+        <p className='letterOpenBtnSubText'>모험가의 편지</p>
+      </button>
       {isVisible ? (
         <div className='letterPopup'>
           <img src={letterImg} width={'100%'} />
